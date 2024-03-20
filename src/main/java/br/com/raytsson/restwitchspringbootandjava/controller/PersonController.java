7059b4f64716eb3a6c33a9078c995f264495e581
raytsson <raytssonmoraes@hotmail.com>
2024-03-20 10:01:24 -0300

    criado o content Negotiation e a serialização em yaml

--- src/main/java/br/com/raytsson/restwitchspringbootandjava/controller/PersonController.java
+++ src/main/java/br/com/raytsson/restwitchspringbootandjava/controller/PersonController.java
@@ -4,8 +4,8 @@ import br.com.raytsson.restwitchspringbootandjava.data.vo.v1.PersonVO;
 import br.com.raytsson.restwitchspringbootandjava.data.vo.v2.PersonVOV2;
 import br.com.raytsson.restwitchspringbootandjava.model.Person;
 import br.com.raytsson.restwitchspringbootandjava.services.PersonServices;
+import br.com.raytsson.restwitchspringbootandjava.util.MediaType;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,28 +19,44 @@ public class PersonController {
     @Autowired
     private PersonServices service;
 
-    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(produces = {MediaType.APPLICATION_JSON
+            , MediaType.APPLICATION_XML
+            , MediaType.APPLICATION_YML})
     public List<PersonVO> findAll() {
         return service.findAll();
     }
-    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "/{id}", produces = {MediaType.APPLICATION_JSON
+            , MediaType.APPLICATION_XML
+            , MediaType.APPLICATION_YML})
     public PersonVO findById(@PathVariable( value = "id") Long id)  throws Exception{
 
         return service.findById(id);
     }
 
-    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE,produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(consumes = {MediaType.APPLICATION_JSON
+            , MediaType.APPLICATION_XML
+            , MediaType.APPLICATION_YML},produces = {MediaType.APPLICATION_JSON
+            , MediaType.APPLICATION_XML
+            , MediaType.APPLICATION_YML})
     public PersonVO create(@RequestBody Person person){
         return service.create(person);
     }
 
-    @PostMapping(value = "/v2",consumes = MediaType.APPLICATION_JSON_VALUE,produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(value = "/v2",consumes = {MediaType.APPLICATION_JSON
+            , MediaType.APPLICATION_XML
+            , MediaType.APPLICATION_YML},produces = {MediaType.APPLICATION_JSON
+            , MediaType.APPLICATION_XML
+            , MediaType.APPLICATION_YML})
     public PersonVOV2 createV2(@RequestBody PersonVOV2 person){
         return service.createv2(person);
     }
 
 
-    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE,produces = MediaType.APPLICATION_JSON_VALUE)
+    @PutMapping(consumes = {MediaType.APPLICATION_JSON
+            , MediaType.APPLICATION_XML
+            , MediaType.APPLICATION_YML},produces = {MediaType.APPLICATION_JSON
+            , MediaType.APPLICATION_XML
+            , MediaType.APPLICATION_YML})
     public PersonVO update(@RequestBody PersonVO person){
 
         return service.update(person);
---
+++ src/main/java/br/com/raytsson/restwitchspringbootandjava/serialization/converter/YamlJacksonHttpMessageConverter.java
@@ -0,0 +1,15 @@
+package br.com.raytsson.restwitchspringbootandjava.serialization.converter;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.AbstractJackson2HttpMessageConverter;
+
+public class YamlJacksonHttpMessageConverter extends AbstractJackson2HttpMessageConverter {
+
+
+    public YamlJacksonHttpMessageConverter() {
+        super(new YAMLMapper().setSerializationInclusion(JsonInclude.Include.NON_NULL),
+                MediaType.parseMediaType("application/x-yaml"));
+    }
+}
